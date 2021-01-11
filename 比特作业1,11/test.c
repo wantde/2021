fb9b8b0865390c7cd4c90d6827482d565fc385b8
1216632952 <1216632952@qq.com>
2021-01-11 14:46:55 +0800

    ok

---
+++ 比特作业1,11/test.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+int main()
+{
+	printf("I lost my cellphone!");
+	int a = 40;
+	int c = 212;
+	
+	printf("%d", (-8 + 22) * a - 10 + c / 2);
+	return 0;
+}
