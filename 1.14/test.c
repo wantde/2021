ce628fc6e957d9da67798faec1d55188dfbf0998
1216632952 <1216632952@qq.com>
2021-01-14 16:39:59 +0800

    我额

---
+++ 1.14/test.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	printf("\73, \32, \99, \97, \110, \32, \100, \111, \32, \105, \116 , \33");
+	return 0;
+}
+
+//int main()
+//{
+//	int a;
+//	float i, j, k;
+//	scanf("%d;%f,%f,%f", &a,&i, &j, &k);
+//	printf("The each subject score of  No. %d is %.2f, %.2f, %.2f.",a,i,j,k);
+//	return 0;
+//}
+//
+
+
+
+//int main()
+//{
+//	
+//	float a=2.324 ;
+//	printf("%o",2799);
+//
+//	return 0;
+//}
+
+
+
+
+
+/*int main()
+{
+	int i, j, k;
+	scanf("%d%d%d", &i, &j, &k);
+	printf("score1=%d,score2=%d,score3=%d",i,j,k);
+	return 0;
+}*/
+
+//int main()
+//{
+//	printf("%d", 0XABCDEF);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+//int i = 1234;
+//int main() {
+//
+//    printf("%o ox%x", i);
+//}
