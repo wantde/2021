bd3f81a2bf9fb8047144900a642be9f774abf1b0
1216632952 <1216632952@qq.com>
2021-01-17 20:12:59 +0800

    1213

---
+++ 1.17-2/test.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	for(  )
+	return 0;
+}
+
+
+
+
+
+
+
+
+//int main()
+//{
+//	int arr[] = { 1,3,5,7,9 };
+//	int size=0;
+//	size = sizeof(arr) / sizeof(arr[0]);
+//	int right=size-1;
+//	int left=0;
+//	int k = 0;
+//	scanf("%d", &k);
+//	while (left<=right)
+//	{
+//		
+//		int mid = (right + left) / 2;
+//		if (k > arr[mid])
+//		{
+//			left = mid + 1;
+//		}
+//		else if (k < arr[mid])
+//		{
+//			right = mid - 1;
+//		}
+//		else if (k == arr[mid])
+//		{
+//			printf("找到了是数组第%d，的%d", mid, k);
+//			break;
+//
+//		}
+//		
+//		
+//	}                
+//	printf("nono");
+//	
+//	
+//	return 0;
+//}
