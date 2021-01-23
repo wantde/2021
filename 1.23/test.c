f3eddd42a3d70a80648403e02a23b22d8106a37b
1216632952 <1216632952@qq.com>
2021-01-23 20:47:25 +0800

    1213

---
+++ 1.23/test.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//int main()
+//{
+//	int k = 0;
+//	scanf("%d", &k);
+//	int i = 0;
+//	int sum = 1;
+//	for (i = 1; i < k + 1; i++)
+//	{
+//		sum = sum * i;
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
+
+
+
+
+
+
+
+//int factorial(int x)
+//{
+//	if (x == 1)
+//	{
+//		return 1;  
+//	}
+//	return x * (factorial(x - 1));
+//}
+//int main()
+//{
+//	int i = 0;
+//	scanf("%d", & i);
+//	int k = factorial(i);
+//	printf("%d", k);
+//	return 0;
+//}
