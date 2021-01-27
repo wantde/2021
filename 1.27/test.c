a6c38437fe68f71c249c78e11182553c7f5f3033
1216632952 <1216632952@qq.com>
2021-01-27 20:39:56 +0800

    1212

---
+++ 1.27/test.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int arr2[10] = { 11,12,13,14,15,16,17,18,19,20 };
+//	for (int i = 0; i < 10; i++)
+//	{
+//		int tmp = arr[i];
+//		arr[i] = arr2[i];
+//		arr2[i] = tmp;
+//	}
+//	return 0;
+//}
+
+
+//void init(int arr[])
+//{
+//	for (int i = 0; i < 10; i++)
+//	{
+//		arr[i] = 0;
+//	}
+//}
+//
+//void print(int arr[])
+//{
+//	for (int i = 0; i < 10; i++)
+//	{
+//		printf("%d\n", arr[i]);
+//	}
+//}
+//void reverse(int arr[], int size)
+//{
+//	for (int i = 0; i < (size / 2); i++)
+//{
+//	int tmp = arr[i];
+//	arr[i] = arr[size - i - 1];
+//	arr[size - 1 - i] = tmp;
+//
+//}
+//}
+//int main()
+//{
+//	int arr[10];
+//	int size = 10;
+//	init(arr);
+//	print(arr);
+//	int ar[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	
+//	reverse(ar, size);
+//	
+//	return 0;
+//}
