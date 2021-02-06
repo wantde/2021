8e807ea382b97459f79400dc04987e342cf0c0d6
1216632952 <1216632952@qq.com>
2021-02-06 21:02:08 +0800

    工业化规划、

---
+++ 2.6/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <string.h>
+int main()
+{
+	/*int arr1[5] = { 0 };
+	int arr2[5] = { 1,2,3 };
+	int arr3[5] = { 1,2,3,'\0' };
+	int arr4[5] = { 'a','c','b' };
+	int arr5[][2][3][4][5][6][5] = { "abc" };
+	printf("%d\n", sizeof(arr1));
+	printf("%d\n", strlen(arr1));
+	printf("%d\n", &arr1[3] - &arr1[0]);
+	printf("hah\n");
+	printf("%d\n", sizeof(arr2));
+	printf("%d\n", strlen(arr2));
+	printf("%d\n", &arr2[3] - &arr2[0]);
+	printf("hah\n");
+	printf("%d\n", sizeof(arr3));
+	printf("%d\n", strlen(arr3));
+	printf("%d\n", &arr3[3] - &arr3[0]);
+	printf("hah\n");
+	printf("%d\n", sizeof(arr4));
+	printf("%d\n", strlen(arr4));
+	printf("%d\n", &arr4[3] - &arr4[0]);
+
+	printf("%d\n", sizeof(arr5));
+	printf("%d\n", strlen(arr5));
+	printf("%d\n", &arr5[3] - &arr5[0]);
+	int a = 0;
+	int******** p = &a;*/
+
+	//3 % 2;
+	//3 % 2.0;下面两行都是错误写法 只可以整数取模 
+	//3.0 % 2;
+	/*printf("%d\n", 3 / 2);
+	printf("%f\n", 3 / 2.0);*/
+	printf("%d", -8 << 1
+	);
+	return 0;
+}
