269250bb336e5fb9f77b0d9bf6d3a08586e5cf6f
1216632952 <1216632952@qq.com>
2021-02-07 20:54:37 +0800

    去

---
+++ 2.7/test.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+
+{
+	 /*int a = 3;
+	 int b = 4;
+	 int c = a & 1;
+	 int d = b & 1;
+	 int e = 8;
+	 int f=e >> 3 & 1;
+	printf("%d\n", c); 
+	printf("%d\n", d);
+	printf("%d\n", f);*/
+	printf("%d", 8 & 9);
+	return 0;
+}
