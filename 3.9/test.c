0be4f21435a6153416598d6b5c0563558bd5c7a6
1216632952 <1216632952@qq.com>
2021-03-09 20:45:21 +0800

    1213

---
+++ 3.9/test.c
@@ -0,0 +1,24 @@
+ #define _CRT_SECURE_NO_WARNINGS
+
+
+#include<stdio.h>
+
+
+
+
+
+
+
+//int main()
+//{
+//	int i = 2;
+//	int a[4][4] = { 0 };
+//	int* p;
+//	int* q[4];
+//     //p = a; 
+//	q[i] = a[i];
+//	p = q[i];  
+//	p = &a[2][1];
+//
+//	return 0;
+//}
